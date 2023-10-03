824fddb91d0c9e6a99940c3c4dbf7734d8dfb086
Caleb Beazel <caleb.beazel@gmail.com>
2023-10-03 14:36:37 -0600

    first-projects 1-2

---
+++ basics_level_1/arrays.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    double temp_list[4] = {34.5, 27.8, 26.8, 22.0};
+
+    std::cout << temp_list[1] << std::endl;
+}
---
+++ basics_level_1/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    double temperature = 20.6; //float
+    bool is_alive = false; //boolean
+    std::string user_name = "Caleb"; //string
+    const int max_run_temp = 76.8; //integer made constant
+
+    std::cout << "Hello" << std::endl;
+    std::cout << temperature << std::endl;
+    return 0;
+}
